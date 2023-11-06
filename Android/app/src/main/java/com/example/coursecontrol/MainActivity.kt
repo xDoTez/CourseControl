0767f5a175a3af3615e8e4f8fc2fc780586e8f31
tsmetisko <tsmetisko@foi.hr>
2023-11-06 18:28:02 +0100

    Created initial Login form

--- Android/app/src/main/java/com/example/coursecontrol/MainActivity.kt
+++ Android/app/src/main/java/com/example/coursecontrol/MainActivity.kt
@@ -15,29 +15,6 @@ import com.example.coursecontrol.ui.theme.CourseControlTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            CourseControlTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CourseControlTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
