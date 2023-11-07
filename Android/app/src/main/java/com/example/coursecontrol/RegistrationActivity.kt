6c2b0e3b209826f084fc04226e58ebfba233be5e
leonspiranec19 <lspiranec@foi.hr>
2023-11-07 17:41:38 +0100

    Corrected misspelling errors

--- Android/app/src/main/java/com/example/coursecontrol/RegistrationActivity.kt
+++ Android/app/src/main/java/com/example/coursecontrol/RegistrationActivity.kt
@@ -7,7 +7,7 @@ import android.widget.Toast
 
 class RegistrationActivity : MainActivity(){
     private lateinit var etName: EditText
-    private lateinit var etSurname: EditText
+    private lateinit var etUsername: EditText
     private lateinit var etEmail: EditText
     private lateinit var etPassword: EditText
     private lateinit var btnRegister: Button
@@ -16,7 +16,7 @@ class RegistrationActivity : MainActivity(){
         setContentView(R.layout.registration)
 
         etName = findViewById(R.id.etName)
-        etSurname = findViewById(R.id.etSurnname)
+        etUsername = findViewById(R.id.etUsername)
         etEmail = findViewById(R.id.etEmail)
         etPassword = findViewById(R.id.etPassword)
         btnRegister = findViewById(R.id.btnRegister)
@@ -27,13 +27,13 @@ class RegistrationActivity : MainActivity(){
     }
     private fun registerUser() {
         val name = etName.text.toString()
-        val surname = etSurname.text.toString()
+        val username = etUsername.text.toString()
         val email = etEmail.text.toString()
         val password = etPassword.text.toString()
 
         // API poziv...
 
-        val message = "Name: $name\nSurname: $surname\nEmail: $email\nPassword: $password"
+        val message = "Name: $name\nUsername: $username\nEmail: $email\nPassword: $password"
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
