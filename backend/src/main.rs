3ad8a0d8e9c30c978594a403db355e085a295eca
Petar-Belcar <petar.belcar@gmail.com>
2023-11-07 15:34:38 +0100

    Added route for user registration

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -42,8 +42,16 @@ async fn get_user_by_id(id: i32) -> Json<Result<users::User, String>>
     Json(users::User::get_user_by_id(id).await)
 }
 
+#[post("/registration", format = "json", data = "<user_credentials>")]
+async fn register_user(user_credentials: Json<users::UserCredentials>) -> Json<users::UserRegistrationResult>
+{
+    let user_credentials = user_credentials.into_inner();
+
+    Json(users::User::register_user(user_credentials).await)
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![status])
-        .mount("/users/", routes![get_user_by_id])
+        .mount("/users/", routes![get_user_by_id, register_user])
 }
