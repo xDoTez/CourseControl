3a7cc2077693708b383af44929327d36b02f5c32
Petar-Belcar <petar.belcar@gmail.com>
2023-11-19 10:04:10 +0100

    Formalized the user registration respons

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -8,7 +8,7 @@ mod regex_checks;
 
 use sqlx::Row;
 
-use rocket::serde::json::Json;
+use rocket::serde::{json::Json, Serialize};
 
 #[get("/")]
 async fn status() -> Json<Result<String, String>>
@@ -42,12 +42,18 @@ async fn get_user_by_id(id: i32) -> Json<Result<users::User, String>>
     Json(users::User::get_user_by_id(id).await)
 }
 
+#[derive(Serialize)]
+struct UserRegistrationResult
+{
+    status: users::UserRegistrationResult 
+}
+
 #[post("/registration", format = "json", data = "<user_credentials>")]
-async fn register_user(user_credentials: Json<users::UserCredentials>) -> Json<users::UserRegistrationResult>
+async fn register_user(user_credentials: Json<users::UserCredentials>) -> Json<UserRegistrationResult>
 {
     let user_credentials = user_credentials.into_inner();
 
-    Json(users::User::register_user(user_credentials).await)
+    Json(UserRegistrationResult{ status: users::User::register_user(user_credentials).await})
 }
 
 #[post("/login", format = "json", data = "<user_login_credentials>")]
