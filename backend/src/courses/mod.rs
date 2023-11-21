ff4d55dae35e86e80f8db8fe75e918967107f2b9
Petar-Belcar <petar.belcar@gmail.com>
2023-11-21 12:33:36 +0100

    Added logic activity check

--- backend/src/courses/mod.rs
+++ backend/src/courses/mod.rs
@@ -83,8 +83,9 @@ pub async fn get_all_course_for_user(session_token: session_token::SessionToken,
 
 async fn get_user_course_data(session_token: session_token::SessionToken, is_active: bool, connection: &mut PgConnection) -> Result<Vec<UserCourse>, String>
 {
-    let user_course: Vec<UserCourse> = match sqlx::query_as("SELECT * FROM user_courses WHERE user_id = $1")
+    let user_course: Vec<UserCourse> = match sqlx::query_as("SELECT * FROM user_courses WHERE user_id = $1 AND is_active = $2")
         .bind(&session_token.user)
+        .bind(&is_active)
         .fetch_all(connection)
         .await
     {
