b4bac0d99c5b6b6c5b77ee975985d1b3482f29c4
Roberto4231 <robertomilakovic1234@gmail.com>
2023-11-22 21:57:00 +0100

    Implemented logout function, quick fix needed after the login functionality has been implemented

--- Android/app/src/main/java/com/example/coursecontrol/MainActivity.kt
+++ Android/app/src/main/java/com/example/coursecontrol/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.coursecontrol
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.coursecontrol.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+
+            when(it.itemId){
+                R.id.logout -> logoutUser()
+
+            }
+            true
+        }
     }
+
+    private fun logoutUser(){
+        TODO("forget user")
+        /*val loginActivityIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginActivityIntent);*/
+    }
+
+
 }
