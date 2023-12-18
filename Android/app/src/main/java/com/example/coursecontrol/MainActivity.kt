14f2ee993e666b71d31d2aac05a5edca1fccb7ca
tsmetisko <tsmetisko@foi.hr>
2023-12-18 15:36:54 +0100

    Main activity fix

--- Android/app/src/main/java/com/example/coursecontrol/MainActivity.kt
+++ Android/app/src/main/java/com/example/coursecontrol/MainActivity.kt
@@ -4,11 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.coursecontrol.ui.theme.CourseControlTheme
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,34 +26,5 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this@MainActivity, RegistrationActivity::class.java)
             startActivity(intent)
         }
-    }
-}
-/* class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            CourseControlTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-} */
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CourseControlTheme {
-        Greeting("Android")
     }
 }
