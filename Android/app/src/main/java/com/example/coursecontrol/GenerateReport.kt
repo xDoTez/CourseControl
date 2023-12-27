3c06a04c32b80c0ec1902d0532961283982e8242
Roberto4231 <robertomilakovic1234@gmail.com>
2023-12-27 20:15:30 +0100

    Changed interface for modules

--- Android/app/src/main/java/com/example/coursecontrol/GenerateReport.kt
+++ Android/app/src/main/java/com/example/coursecontrol/GenerateReport.kt
@@ -1,6 +1,5 @@
 package com.example.coursecontrol
 
-import android.app.Activity
 import android.content.Context
 import android.graphics.drawable.Drawable
 import com.example.coursecontrol.model.CourseData
@@ -8,6 +7,5 @@ import com.example.coursecontrol.model.CourseData
 interface GenerateReport {
     fun getIcon(context: Context) : Drawable
     fun getName(context: Context) : String
-    fun getActivity() : Activity
     fun setData(courseDataList: List<CourseData>)
 }
