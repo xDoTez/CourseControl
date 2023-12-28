a2eb3899ba0bb790a5949aa5375b757288bba8be
Roberto4231 <robertomilakovic1234@gmail.com>
2023-12-28 17:14:30 +0100

    Added method generateReport to interface for modules

--- Android/app/src/main/java/com/example/coursecontrol/GenerateReport.kt
+++ Android/app/src/main/java/com/example/coursecontrol/GenerateReport.kt
@@ -5,6 +5,7 @@ import android.graphics.drawable.Drawable
 import com.example.coursecontrol.model.CourseData
 
 interface GenerateReport {
+    fun generateReport()
     fun getIcon(context: Context) : Drawable
     fun getName(context: Context) : String
     fun setData(courseDataList: List<CourseData>)
