ae04b5455d23912117368bc959f7e7f0ea82b9f4
leonspiranec19 <lspiranec@foi.hr>
2024-01-08 11:11:55 +0100

    Getting non-admin users fix

--- Android/app/src/main/java/com/example/coursecontrol/model/ApiResponseAdminPrivileges.kt
+++ Android/app/src/main/java/com/example/coursecontrol/model/ApiResponseAdminPrivileges.kt
@@ -1,6 +1,6 @@
 package com.example.coursecontrol.model
 
-class ApiResponseAdminPrivileges(
+data class ApiResponseAdminPrivileges(
     val status: String,
     val message: String?,
     val users: List<User>
