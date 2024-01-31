36ec26c5b4f5ecdc5d5547d8126fbd8444fac840
xDoTez <dtezak@foi.hr>
2024-01-31 13:57:50 +0100

    Added different view for history detailed view and navigation to detailed_course_view.xml

---
+++ Android/core/src/main/java/hr/foi/air/core/GenerateReport.kt
@@ -0,0 +1,12 @@
+package hr.foi.air.core
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import com.example.coursecontrol.model.CourseData
+
+interface GenerateReport {
+    fun generateReport()
+    fun getIcon(context: Context) : Drawable
+    fun getName(context: Context) : String
+    fun setData(courseDataList: List<CourseData>)
+}
